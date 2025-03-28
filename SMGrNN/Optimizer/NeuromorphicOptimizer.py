09deb79b6fec31889c52d4ee3cbaa7be8fd4c3db
ShatteredXZ <804537158@qq.com>
2025-03-28 15:20:42 +0000

    Update: Growth based on Neuron Activity

--- SMGrNN/Optimizer/NeuromorphicOptimizer.py
+++ SMGrNN/Optimizer/NeuromorphicOptimizer.py
@@ -12,7 +12,7 @@ class NeuromorphicOptimizer:
         self.edge_weight = edge_weight
         self.edge_weight_previous = edge_weight.clone()
         ## =========================  Parameters  =========================
-        self.activity_threshold = 0.75         
+        self.activity_threshold = torch.tensor(0.68)        # threshold for node activity
         ## =========================  Data  =========================
         self.node_activity_history = []        # history of node activity（Tensor）
         self.edge_derivative_history = []        # history of edge weight derivative（Tensor）
@@ -24,7 +24,7 @@ class NeuromorphicOptimizer:
         Update node activity and store historical info for future growth decision
         """
         # 当前节点活动：特征求和
-        self.node_activity = self.g.nodes.sum(dim=1)
+        self.node_activity = self.g.nodes.mean(dim=1)
 
         # 记录历史节点活动（detach 避免占用计算图内存）
         self.node_activity_history.append(self.node_activity.detach().clone())
@@ -43,14 +43,27 @@ class NeuromorphicOptimizer:
         """
         Grow the network based on the historical information
         """
+        new_nodes_source_indices = []
+        new_edges = torch.zeros(2,0).long()
         # Check if the network is ready to grow
         if self.timesteps < self.history_window:
             return
         # Calculate the average node activity
-        avg_activity = torch.stack(torch.abs(self.node_activity_history)).mean(dim=0)
+        # Convert the list of tensors to a single tensor
+        node_activity_tensor = torch.stack(self.node_activity_history)
+        # Calculate the average node activity (taking absolute values)
+        avg_activity = torch.abs(node_activity_tensor).mean(dim=0)
         # Add new nodes from active nodes
-        active_nodes = avg_activity > torch.max(avg_activity.mean()+2*avg_activity.std(),self.activity_threshold)
-        ...
+        active_nodes = avg_activity > torch.max(avg_activity.mean()+0.5*avg_activity.std(),self.activity_threshold)
+        num_new_nodes = torch.sum(active_nodes).item()
+        if num_new_nodes != 0:
+            new_nodes_source_indices.append(torch.nonzero(active_nodes).view(-1).tolist())
+            for i in new_nodes_source_indices[0]:
+                new_edges = torch.cat([new_edges,torch.tensor([[i],[self.g.nodes.size(0)+i]],dtype=torch.long)],dim=1)
         
+        #TODO: Add new edges from the edge derivative
+        edge_derivative_tensor = torch.stack(self.edge_derivative_history)
+            
+
 
         
